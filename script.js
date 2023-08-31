9773512af53e0c621ae3fec21a3649c9b035700f
Rahul Kumar <114710940+rahulsingh73679@users.noreply.github.com>
2023-08-31 13:14:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
- // typing text animation script var typed=new Typed(".typing",{strings: ["Engineer","Developer", "Founder", "App Developer", "Freelancer","Data Scientist"], typeSpeed: 100, backSpeed: 60, cursorChar: ' |', smartBackspace: true, loop: true});
+ // typing text animation script 
+var typed=new Typed(".typing",{strings: ["Engineer","Developer", "Founder", "App Developer", "Freelancer","Data Scientist"], typeSpeed: 100, backSpeed: 60, cursorChar: ' |', smartBackspace: true, loop: true});
