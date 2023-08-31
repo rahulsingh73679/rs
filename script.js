2f981fe044d5d75ae5561947f350494b79da8d96
Rahul Kumar <114710940+rahulsingh73679@users.noreply.github.com>
2023-08-31 13:16:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
- // typing text animation script 
-var typed=new Typed(".typing",{strings: ["Engineer","Developer", "Founder", "App Developer", "Freelancer","Data Scientist"], typeSpeed: 100, backSpeed: 60, cursorChar: ' |', smartBackspace: true, loop: true});
+ var typed = new Typed(".typing", {
+        strings: ["Engineer","Developer", "Founder", "App Developer", "Freelancer","Data Scientist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
